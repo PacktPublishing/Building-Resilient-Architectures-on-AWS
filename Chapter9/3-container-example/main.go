8020e9fe86e5e718eb5bf8866e54fa10a7d21181
packt-irfaa <116081669+packt-irfaa@users.noreply.github.com>
2024-11-04 14:01:22 +0530

    Update main.go

--- Chapter9/3-container-example/main.go
+++ Chapter9/3-container-example/main.go
@@ -13,8 +13,8 @@ type Response struct {
 func main() {
 	http.HandleFunc("/", handleRequest)
 
-	log.Println("Starting server on :8000")
-	err := http.ListenAndServe(":8000", nil)
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
